92c1a361f063af85e60d7961a76e12f5151e074e
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-10-03 23:21:10 +0200

    Create Hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,31 @@
+<script setup>
+import frog from "@/assets/DALL_E_Frog1.png";
+import vector from "@/assets/vector-background-1.svg";
+</script>
+
+<template>
+  <section class="pt-24 flex justify-center">
+    <div class="flex gap-10">
+      <div class="flex flex-col justify-center items-start gap-10">
+        <h1
+          class="font-extrabold text-3xl bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text"
+        >
+          FULFILL YOUR DREAM<br />GET A FROG
+        </h1>
+        <button
+          class="btn bg-gradient-to-r from-primary to-accent text-light font-bold text-xl"
+        >
+          Browse Frogs
+        </button>
+      </div>
+      <div class="relative">
+        <img
+          :src="frog"
+          alt="beautidul blue frog"
+          class="w-96 absolute right-20 bottom-20"
+        />
+        <img :src="vector" alt="vector background" class="w-96" />
+      </div>
+    </div>
+  </section>
+</template>
