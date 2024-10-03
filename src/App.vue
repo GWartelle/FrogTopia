d1ccf5d2b52d19d3270a89110970bc99052deded
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-10-03 14:58:42 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import logo from "@/assets/frog-logo.svg";
+</script>
+
+<template>
+  <img :src="logo" class="w-10" />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0A8754",
+        secondary: "#C5E4D7",
+        accent: "#DEB841",
+        dark: "#002A19",
+        light: "#F4F4F6",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
