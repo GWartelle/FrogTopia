fe9df4c6873b9b5fa1a5194249c388f6b6ea87f8
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-10-06 10:49:29 +0200

    Update Hero component layout

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,11 +1,11 @@
 <script setup>
-import frog from "@/assets/DALL_E_Frog1.png";
-import vector from "@/assets/vector-background-1.svg";
+import frog1 from "@/assets/DALL_E_Frog1.png";
+import vector1 from "@/assets/vector-background-1.svg";
 </script>
 
 <template>
-  <section class="pt-24 flex justify-center">
-    <div class="flex gap-10">
+  <section class="pt-24 h-screen flex justify-center">
+    <div class="flex gap-12 items-center">
       <div class="flex flex-col justify-center items-center gap-10">
         <h1
           class="font-extrabold text-3xl bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text"
@@ -13,18 +13,18 @@ import vector from "@/assets/vector-background-1.svg";
           FULFILL YOUR DREAM<br />GET A FROG
         </h1>
         <button
-          class="btn bg-gradient-to-r from-primary to-accent text-light font-bold text-xl"
+          class="btn border-none bg-gradient-to-r from-primary to-accent text-light font-bold text-xl shadow-md"
         >
           Browse Frogs
         </button>
       </div>
       <div class="relative">
         <img
-          :src="frog"
+          :src="frog1"
           alt="beautidul blue frog"
           class="w-96 absolute right-14 bottom-14"
         />
-        <img :src="vector" alt="vector background" class="w-96" />
+        <img :src="vector1" alt="vector background" class="w-96" />
       </div>
     </div>
   </section>
