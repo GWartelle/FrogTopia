6d57a863e69f9945a65a0c75dda73e583b515a47
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-10-06 11:50:58 +0200

    Create Browse section

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
 import Promo from "./components/Promo.vue";
+import Browse from "./components/Browse.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
   <Promo />
+  <Browse />
 </template>
---
+++ src/components/Browse.vue
@@ -0,0 +1,18 @@
+<scrip setup>
+</scrip>
+
+<template>
+  <section class="flex flex-col items-center gap-10 py-10">
+    <h2
+      class="font-extrabold text-3xl bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text"
+    >
+      Browse Frogs
+    </h2>
+    <label
+      class="input input-bordered flex items-center gap-2 rounded-full border-dark"
+    >
+      <input type="text" class="grow" placeholder="Search for a frog..." />
+      <v-icon name="io-search-outline" />
+    </label>
+  </section>
+</template>
