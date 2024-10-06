ce90ffb54b9de0ef914f44281a46186b10495513
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-10-06 14:01:44 +0200

    Create Card component

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,5 +1,12 @@
-<scrip setup>
-</scrip>
+<script setup>
+import Card from "./Card.vue";
+import frog from "../assets/frogs/blue-frog.jpg";
+
+const alt = "This is a frog";
+const title = "THIS IS A BLUE FROG";
+const text = "THIS BLUE FROG IS BLUE";
+const price = 999.99;
+</script>
 
 <template>
   <section class="flex flex-col items-center gap-10 py-10">
@@ -14,5 +21,13 @@
       <input type="text" class="grow" placeholder="Search for a frog..." />
       <v-icon name="hi-search" fill="#002A19" />
     </label>
+    <Card
+      class="bg-secondary rounded-lg shadow-md"
+      :image="frog"
+      :alt="alt"
+      :title="title"
+      :text="text"
+      :price="price"
+    />
   </section>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-3 px-3 py-5">
+    <img
+      :src="image"
+      :alt="alt"
+      class="h-44 w-44 object-cover rounded-lg shadow-md"
+    />
+    <h3 class="text-dark font-bold text-xl">{{ title }}</h3>
+    <p class="text-dark text-center">{{ text }}</p>
+    <span class="text-dark font-bold text-2xl">${{ price }}</span>
+    <div class="flex gap-5">
+      <button
+        class="btn border-none bg-primary text-light font-bold text-lg shadow-md"
+      >
+        Read more
+      </button>
+      <button
+        class="btn border-none bg-primary text-light font-bold text-lg shadow-md"
+      >
+        Add to cart
+      </button>
+    </div>
+  </div>
+</template>
